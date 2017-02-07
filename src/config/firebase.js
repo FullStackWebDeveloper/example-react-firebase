cdd868b88d608b363b402d471decb9de9f331999
piterpansmile <piterpan.smile@gmail.com>
2017-02-07 22:27:14 +0100

    example-react-firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,7 @@
+export default {
+  apiKey: 'PUT_FIREBASE_API_KEY_HERE',
+  authDomain: 'PUT_FIREBASE_AUTH_DOMAIN_HERE',
+  databaseURL: 'PUT_FIREBASE_DATABASE_URL_HERE',
+  storageBucket: 'PUT_FIREBASE_STORAGE_BUCKET_HERE',
+  messagingSenderId: 'PUT_FIREBASE_MESSAGING_SENDER_ID_HERE',
+};
